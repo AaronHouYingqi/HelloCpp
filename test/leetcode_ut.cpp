d813ba891f502ad79cdb5088b142efd709a9ce3b
Yingqi <AaronHouYingqi@qq.com>
2025-05-23 21:24:51 +0800

    Update leetcode_ut.cpp.

--- test/leetcode_ut.cpp
+++ test/leetcode_ut.cpp
@@ -2,14 +2,44 @@
  * 
  */
 
-#include "gtest/gtest.h"
-// #include "gmock/gmock.h"
+ // TODO #include <gmock/gmock.h>
+#include <gtest/gtest.h>  // FIXME installed?
+#include <unordered_set>
+
 #include "solution.h"
 
-// TODO GoogleTest class
-
-TEST(leetcode, case_0)
+TEST(Demo, UniqueSolution)
 {
     int ret = foo();
-    EXPECT_EQ(0, ret);
+    EXPECT_EQ(ret, 0);
+}
+
+TEST(Demo, MultipleSolutions) {
+    int ret = foo();
+    unordered_set<int> ret_set = {0, 1};
+    EXPECT_TRUE(ret_set.contains(ret));
+}
+
+class LeetCodeTest : public testing::Test {
+    protected:
+    LeetCodeTest() { }
+    ~LeetCodeTest() override { }
+    void SetUp() override { }
+    void TearDown() override { }
+    Solution solution;
+};
+
+TEST_F(LeetCodeTest, Testcase1) {
+    int ret = foo();
+    EXPECT_EQ(ret, 0);
+}
+
+TEST_F(LeetCodeTest, Testcase2) {
+    int ret = foo();
+    EXPECT_EQ(ret, 0);
+}
+
+TEST_F(LeetCodeTest, Testcase3) {
+    int ret = foo();
+    EXPECT_EQ(ret, 0);
 }
