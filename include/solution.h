90387eea95e5c73f86579e56a1ab7396d376db76
Yingqi <AaronHouYingqi@qq.com>
2025-05-23 21:17:48 +0800

    Update solution.h.

--- include/solution.h
+++ include/solution.h
@@ -5,6 +5,18 @@
 #ifndef SOLUTION_H
 #define SOLUTION_H
 
+#include <iostream>
+
+using namespace std;
+
 int foo();
+
+class Solution {
+public:
+    int foo() {
+        cout << "foo" << endl;
+        return 0;
+    }
+};
 
 #endif  // SOLUTION_H
